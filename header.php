0f738b94795c885ad3cea93f42722bb67ce58d0b
Brieuc Fresnel <brieuc@studio-dot.fr>
2022-05-27 17:59:21 +0200

    Add base menu & menu toggle

--- header.php
+++ header.php
@@ -22,3 +22,5 @@
 <body <?php body_class() ?>>
 <?php wp_body_open() ?>
 
+<?php get_template_part('templates/menu') ?>
+
---
+++ templates/menu.php
@@ -0,0 +1,17 @@
+<div class="main-menu">
+    <div class="container">
+        <div class="menu-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="main-menu__logo">
+
+        </div>
+        <div class="main-menu__navbar">
+            Test
+        </div>
+        <div>&nbsp;</div>
+    </div>
+
+</div>
