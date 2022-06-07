54c638dcd664ddaa20380e6b3a8b3f663b73ae4e
Brieuc Fresnel <brieuc@studio-dot.fr>
2022-06-07 14:24:09 +0200

    Indent helpers.php

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,25 +1,26 @@
 <?php
 
+
 /**
  * is_edit_page
  * function to check if the current page is a post edit page
  *
+ * @param string $new_edit what page to check for accepts new - new post page ,edit - edit post page, null for either
+ * @return boolean
  * @author Ohad Raz <admin@bainternet.info>
  *
- * @param  string  $new_edit what page to check for accepts new - new post page ,edit - edit post page, null for either
- * @return boolean
  */
-function is_edit_page($new_edit = null){
+function is_edit_page($new_edit = null) {
     global $pagenow;
     //make sure we are on the backend
     if (!is_admin()) return false;
 
-    if($new_edit == "edit")
-        return in_array( $pagenow, array( 'post.php',  ) );
-    elseif($new_edit == "new") //check for new post page
-        return in_array( $pagenow, array( 'post-new.php' ) );
+    if ($new_edit == "edit")
+        return in_array($pagenow, array('post.php',));
+    elseif ($new_edit == "new") //check for new post page
+        return in_array($pagenow, array('post-new.php'));
     else //check for either new or edit
-        return in_array( $pagenow, array( 'post.php', 'post-new.php' ) );
+        return in_array($pagenow, array('post.php', 'post-new.php'));
 }
 
 /**
@@ -29,10 +30,10 @@ function is_edit_page($new_edit = null){
  * @return boolean
  */
 function is_edit_page_type_page(): bool {
-    if(!is_edit_page())
+    if (!is_edit_page())
         return false;
 
-    if(get_current_screen() && get_current_screen()->post_type === 'page')
+    if (get_current_screen() && get_current_screen()->post_type === 'page')
         return true;
 
     return false;
