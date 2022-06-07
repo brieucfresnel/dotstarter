b1fc00ebc93e701cad8f180a7919dd407ff155ff
Brieuc Fresnel <brieuc@studio-dot.fr>
2022-06-07 14:24:17 +0200

    Add "Event" post type for testing purposes

--- includes/post-types.php
+++ includes/post-types.php
@@ -8,30 +8,30 @@ if(!class_exists('DOT_PostTypes')) {
 
         public function register_post_types() {
             $labels = array(
-                'name'                  => _x( 'Products', 'Post type general name', 'dotstarter' ),
-                'singular_name'         => _x( 'Product', 'Post type singular name', 'dotstarter' ),
-                'menu_name'             => _x( 'Products', 'Admin Menu text', 'dotstarter' ),
-                'name_admin_bar'        => _x( 'Product', 'Add New on Toolbar', 'dotstarter' ),
+                'name'                  => _x( 'Events', 'Post type general name', 'dotstarter' ),
+                'singular_name'         => _x( 'Event', 'Post type singular name', 'dotstarter' ),
+                'menu_name'             => _x( 'Events', 'Admin Menu text', 'dotstarter' ),
+                'name_admin_bar'        => _x( 'Event', 'Add New on Toolbar', 'dotstarter' ),
                 'add_new'               => __( 'Add New', 'dotstarter' ),
-                'add_new_item'          => __( 'Add New Product', 'dotstarter' ),
-                'new_item'              => __( 'New Product', 'dotstarter' ),
-                'edit_item'             => __( 'Edit Product', 'dotstarter' ),
-                'view_item'             => __( 'View Product', 'dotstarter' ),
-                'all_items'             => __( 'All Products', 'dotstarter' ),
-                'search_items'          => __( 'Search Products', 'dotstarter' ),
-                'parent_item_colon'     => __( 'Parent Products:', 'dotstarter' ),
-                'not_found'             => __( 'No products found.', 'dotstarter' ),
-                'not_found_in_trash'    => __( 'No products found in Trash.', 'dotstarter' ),
-                'featured_image'        => _x( 'Product Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'dotstarter' ),
+                'add_new_item'          => __( 'Add New Event', 'dotstarter' ),
+                'new_item'              => __( 'New Event', 'dotstarter' ),
+                'edit_item'             => __( 'Edit Event', 'dotstarter' ),
+                'view_item'             => __( 'View Event', 'dotstarter' ),
+                'all_items'             => __( 'All Events', 'dotstarter' ),
+                'search_items'          => __( 'Search Events', 'dotstarter' ),
+                'parent_item_colon'     => __( 'Parent Events:', 'dotstarter' ),
+                'not_found'             => __( 'No events found.', 'dotstarter' ),
+                'not_found_in_trash'    => __( 'No events found in Trash.', 'dotstarter' ),
+                'featured_image'        => _x( 'Event Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'dotstarter' ),
                 'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'dotstarter' ),
                 'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'dotstarter' ),
                 'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'dotstarter' ),
-                'archives'              => _x( 'Product archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'dotstarter' ),
-                'insert_into_item'      => _x( 'Insert into product', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'dotstarter' ),
-                'uploaded_to_this_item' => _x( 'Uploaded to this product', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'dotstarter' ),
-                'filter_items_list'     => _x( 'Filter products list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'dotstarter' ),
-                'items_list_navigation' => _x( 'Products list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'dotstarter' ),
-                'items_list'            => _x( 'Products list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'dotstarter' ),
+                'archives'              => _x( 'Event archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'dotstarter' ),
+                'insert_into_item'      => _x( 'Insert into event', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'dotstarter' ),
+                'uploaded_to_this_item' => _x( 'Uploaded to this event', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'dotstarter' ),
+                'filter_items_list'     => _x( 'Filter events list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'dotstarter' ),
+                'items_list_navigation' => _x( 'Events list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'dotstarter' ),
+                'items_list'            => _x( 'Events list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'dotstarter' ),
             );
 
             $args = array(
@@ -41,15 +41,15 @@ if(!class_exists('DOT_PostTypes')) {
                 'show_ui'            => true,
                 'show_in_menu'       => true,
                 'query_var'          => true,
-                'rewrite'            => array( 'slug' => 'product' ),
+                'rewrite'            => array( 'slug' => 'event' ),
                 'capability_type'    => 'post',
                 'has_archive'        => true,
                 'hierarchical'       => false,
                 'menu_position'      => null,
-                'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+                'supports'           => array( 'title', 'thumbnail', 'excerpt' ),
             );
 
-            register_post_type( 'product', $args );
+            register_post_type( 'event', $args );
 
 //            Example of Post Type arguments
 //
