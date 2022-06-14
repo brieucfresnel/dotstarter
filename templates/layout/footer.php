dfa4ebb1dee466a4d4448a344f79af7052fbdba1
Brieuc Fresnel <brieuc@studio-dot.fr>
2022-06-14 15:53:47 +0200

    Move footer template to templates/layout/footer

--- templates/layout/footer.php
+++ templates/layout/footer.php
@@ -1,1 +1,16 @@
-<?php get_template_part('templates/layout/footer');
+<div class="footer">
+    <div class="container">
+        <h2>Footer</h2>
+    </div>
+</div>
+
+<?php wp_footer(); ?>
+
+</div>
+
+<?php if (WP_DEBUG): ?>
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.27.9'><\/script>".replace("HOST", location.hostname));
+    <?php endif; ?>
+    </body>
+    </html>
