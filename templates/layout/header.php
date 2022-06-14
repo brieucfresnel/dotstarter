d63aefc247f8b6a0beb7f6bd31fe0e28db79608e
Brieuc Fresnel <brieuc@studio-dot.fr>
2022-06-14 15:57:45 +0200

    Add transition panels + barba markup to header

--- templates/layout/header.php
+++ templates/layout/header.php
@@ -5,12 +5,12 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= DOT_THEME_PATH ?>/assets/img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= DOT_THEME_PATH ?>/assets/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= DOT_THEME_PATH ?>/assets/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?= DOT_THEME_PATH ?>/assets/img/favicon/site.webmanifest">
-    <link rel="mask-icon" href="<?= DOT_THEME_PATH ?>/assets/img/favicon/safari-pinned-tab.svg" color="#ed302e">
-    <link rel="shortcut icon" href="<?= DOT_THEME_PATH ?>/assets/img/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= DOT_THEME_PATH ?>/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= DOT_THEME_PATH ?>/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= DOT_THEME_PATH ?>/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= DOT_THEME_PATH ?>/assets/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?= DOT_THEME_PATH ?>/assets/favicon/safari-pinned-tab.svg" color="#ed302e">
+    <link rel="shortcut icon" href="<?= DOT_THEME_PATH ?>/assets/favicon/favicon.ico">
     <meta name="msapplication-TileColor" content="#000000">
     <meta name="msapplication-config" content="<?= DOT_THEME_PATH ?>/assets/img/favicon/browserconfig.xml">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -19,8 +19,20 @@
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class() ?>>
+<body <?php body_class() ?> data-barba="wrapper">
 <?php wp_body_open() ?>
 
-<?php get_template_part('templates/layout/menu');
+<input data-url="<?php echo get_template_directory_uri(); ?>" id="templateURL" type="hidden" name="templateURL">
 
+<div id="panel3">
+    <div id="containerLottieLoading"></div>
+</div>
+
+<div id="transition">
+    <div id="panel2"></div>
+    <div id="panel1"></div>
+</div>
+
+<?php get_template_part('templates/layout/menu'); ?>
+
+<div id="main" data-barba="container">
