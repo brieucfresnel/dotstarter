29bac5e6a3724c3e72d9a9877577028631a7d221
Brieuc Fresnel <brieuc@studio-dot.fr>
2022-08-22 15:43:52 +0200

    Add gsap-loader.js to register gsap plugins on launch

--- assets/js/lib/gsap-loader.js
+++ assets/js/lib/gsap-loader.js
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
 import { SplitText } from "gsap/SplitText";
+// import SmoothScroll from "gsap/ScrollSmoother";
 
 export default function() {
     gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, SplitText);
